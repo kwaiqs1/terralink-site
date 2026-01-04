1eaf9655478fd65c07490411c975b1836b1c40c4
kwaiqs1 <nurken4ik2010@gmail.com>
2026-01-05 04:30:35 +0500

    Update settings.py

--- terralink_site/settings.py
+++ terralink_site/settings.py
@@ -15,7 +15,10 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-producti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = [
+    "terralink.up.railway.app",
+    ".railway.app",
+]
 
 
 # Application definition

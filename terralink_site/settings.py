5c8ab2f5ee4c761ed325cfb46476f23a4ac9f48a
kwaiqs1 <nurken4ik2010@gmail.com>
2026-01-05 10:14:54 +0500

    Update settings.py

--- terralink_site/settings.py
+++ terralink_site/settings.py
@@ -76,10 +76,9 @@ WSGI_APPLICATION = 'terralink_site.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
-    )
+    "default": dj_database_url.config(default="sqlite:///db.sqlite3")
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [

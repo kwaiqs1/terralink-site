48b581264a4076d3e8399d35c1e97a0057ab065b
kwaiqs1 <nurken4ik2010@gmail.com>
2026-01-05 12:51:36 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,9 @@ echo "PORT is: ${PORT:-8000}"
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+# ✅ Create superuser from Railway Variables (safe to run every deploy)
+python manage.py ensure_superuser
+
 exec gunicorn terralink_site.wsgi:application \
   --bind "0.0.0.0:${PORT:-8000}" \
   --access-logfile - \
